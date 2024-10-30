3ee57bf149bab47b989546789eb4d6af019bb4eb
Jniamke12 <162131770+Jniamke12@users.noreply.github.com>
2024-10-30 23:16:15 +0000

    parametre2

--- Reseau_Social/Reseau_Social/settings.py
+++ Reseau_Social/Reseau_Social/settings.py
@@ -117,10 +117,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
